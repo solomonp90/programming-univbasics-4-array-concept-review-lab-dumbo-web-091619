9387e6ec900448ae0834d997715a6d4075a02945
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 16:12:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   # expect(find_element_index(scale,2)).to eq(0)
+#i<array.length
 array = [1,2,3,4]
 i = 0
-while  do |index|
+while array[i] do |index|
   p index
 end
 find_element_index([1,2,3,4],0)
