da6bd278a25690b1ea067d79f35ada660a75c3e5
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 16:16:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 #i<array.length
 array = [1,2,3,4]
 i = 0
-while i < array.length do |index|
+4.times do |index|
   puts index
 end
 find_element_index([1,2,3,4],0)
