87b58e52c7bd2047d7ffac48c5425ba83653f191
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 18:49:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def find_element_index(array,value_to_find)
 array.index(value_to_find)
 end
-#find_element_index([2,3,4,5],3)
 
 
 
@@ -19,8 +18,7 @@ end
 
 
 def find_max_value(array)
-  #valley = [5,4,3,2,1,0,1,2,3,4,5]
-  #  hill = [1,2,3,4,5,4,3,2,1]
+  array.max
 end
 
 def find_min_value(array)
