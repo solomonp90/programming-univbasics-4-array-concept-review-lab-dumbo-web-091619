0fecfa14cdaaee5fc4fa847835be2821ec685ad0
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 16:21:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,25 @@
-def find_element_index(array, value_to_find)
-  # expect(find_element_index(scale,2)).to eq(0)
-#i<array.length
-array = [1,2,3,4]
-i = 0
-1.times do |index|
-  puts index
+def find_element_index(array,value_to_find)
+  
+
 end
-find_element_index([1,2,3,4],0)
-end
+
+
+
+
+
+# def find_element_index(array, value_to_find)
+#   # expect(find_element_index(scale,2)).to eq(0)
+# #i<array.length
+# array = [1,2,3,4]
+# i = 0
+# 1.times do |index|
+#   puts index
+# end
+# find_element_index([1,2,3,4],0)
+# end
 
 def find_max_value(array)
   # Add your solution here
-  array.size
 end
 
 def find_min_value(array)
