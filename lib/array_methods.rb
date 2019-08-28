3cbaadf6369d7033919cdbc793193061ac3a2dfe
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 18:52:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,21 +2,6 @@ def find_element_index(array,value_to_find)
 array.index(value_to_find)
 end
 
-
-
-
-
-# scale = [2,3,4,5]
-#  scale2 = [6,5,4,3]
-#  hill = [1,2,3,4,5,4,3,2,1]
-#  valley = [5,4,3,2,1,0,1,2,3,4,5]
-#  zig_zag = [500,4,1000,5,250]
-
-
-
-
-
-
 def find_max_value(array)
   array.max
 end
