b8a1df323925ea4b38d2dd51fca51a60a4dab0a6
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 16:05:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,9 @@ def find_element_index(array, value_to_find)
   # expect(find_element_index(scale,2)).to eq(0)
 index = 0
 4.times do |index|
-
+  p index
+end
+find_element_index([1,2,3,4],0)
 end
 
 def find_max_value(array)
