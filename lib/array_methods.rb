d5579832030b18d71fc24d9ba3ade649f5b3304f
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 17:12:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,10 +5,10 @@ def find_element_index(array,value_to_find)
  valley = [5,4,3,2,1,0,1,2,3,4,5]
  zig_zag = [500,4,1000,5,250]
 
- scale.index();
- scale.index();
- scale.index();
- scale.index();
+ scale.index(2);
+ scale.index(4);
+ scale.index(5);
+ scale.index(6);
  scale.index();
 end
 #
