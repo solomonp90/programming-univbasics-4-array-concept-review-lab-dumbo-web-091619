1f9810ac41dc84b7694c4580eb16f6c9deea0eee
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 18:17:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array,value_to_find)
- valley = [5,4,3,2,1,0,1,2,3,4,5]
-value_to_find = scale.index(0)
-valley.index(0);
+ array = [2,3,4,5]
+#value_to_find = scale.index(0)
+array.index(0);
 
 
 #scale2.index(value_to_find);
@@ -25,7 +25,8 @@ find_element_index([2,3,4,5],2)
 
 
 def find_max_value(array)
-  #zig_zag = [500,4,1000,5,250]
+  #valley = [5,4,3,2,1,0,1,2,3,4,5]
+  #  hill = [1,2,3,4,5,4,3,2,1]
 end
 
 def find_min_value(array)
