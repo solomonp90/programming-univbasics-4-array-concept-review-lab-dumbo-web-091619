494a0b5dc2ed78c1589a42f1fb3c07b0570cdc74
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-28 18:31:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array,value_to_find)
  array = [2,3,4,5]
 value_to_find = 3
-array.index(2);
+array.index(6);
 #scale2.index(value_to_find);
 end
 find_element_index([2,3,4,5],3)
